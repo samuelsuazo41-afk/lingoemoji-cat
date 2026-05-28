43a3427e9d29cc12204a607ea714d46fca7a0f34
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-28 23:48:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 /* ===== LINGOEMOJICAT - SERVICE WORKER ===== */
 
-const CACHE_NAME = 'lingoemojicat-v3';
+const CACHE_NAME = 'lingoemojicat-v7';
 const ASSETS_TO_CACHE = [
   // HTML i PWA
   './',
